18ea72d65ed348d6dc38ec17cbca24217f77f746
malik-abdullah <malik.abdullah185@gmail.com>
2016-04-25 17:41:22 +0500

    Report about Final Year Project.

---
+++ GitHubWork/src/GitHub.java
@@ -0,0 +1,14 @@
+
+public class GitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome Abdullah Malik");
+		System.out.println("How Are You?");
+		System.out.println("What the progress of FYP?");
+		
+		
+			}
+
+}
